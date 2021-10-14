6e5539fbf80674655ae209671f7c307a8aef511f
Artur Oliferuk <45690506+Mamin-simpotiaga@users.noreply.github.com>
2021-10-14 18:34:04 +0300

    Update LiczbyPierwsze.cpp

--- ArturOliferuk/LiczbyPierwsze.cpp
+++ ArturOliferuk/LiczbyPierwsze.cpp
@@ -2,30 +2,29 @@
 #include <ctime>
  
 using namespace std;
-int factorial(int i)
-{
-  if (i==0) return 1;
-  else return i*factorial(i-1);
-}
  
-int main()
+int main(){
+    int q, i;
+cin>>q;
+for (int j=0; j<q; ++j)
 {
-	int q,n;
-    cin>>q;
-    int fakt=1;
-	for (int j=0; j<q; ++j)
-	{
-		cin>>n;
-		if(n>9)
-		{
-			cout<<0<<" "<<0<<endl;
-		}
-		else{
-		
-    	fakt=factorial(n);
-		cout<<(fakt%100)/10<<" "<<fakt%10<<endl;
-		fakt=1;}
-	}
+    cin>>i;
+    if (i==1) {cout<<"NIE"<<endl;}
+    if (i==2) {cout<<"TAK"<<endl;}
 
+        int l;
+        for(l=2; l<i; ++l)
+        {
+            if ((i%l) == 0)
+            {
+                cout<<"NIE"<<endl;
+                break;
+            }
+            if (l == (i-1))
+            {
+                cout<<"TAK"<<endl;
+            }
+        }
+}
     return 0;
 }
