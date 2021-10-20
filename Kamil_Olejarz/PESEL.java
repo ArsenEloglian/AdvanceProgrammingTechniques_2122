ea33e1f11a9c508e0b032daa991ff7ed72d346d7
sa20z-7 <sa20z-7@dmcs.pl>
2021-10-20 23:04:37 +0200

    poprawa zadania pesel

--- Kamil_Olejarz/PESEL.java
+++ Kamil_Olejarz/PESEL.java
@@ -5,31 +5,29 @@ public class PESEL {
 
     public static void main(String[] args){
 
-            String pesel = null;
-            String[] tab = new String[0];
-            int iloscTestow = 0;
-            int res = 0;
+        String pesel = null;
+        String[] tab = new String[0];
+        int iloscTestow = 0;
+        int res = 0;
 
-            Scanner scanner = new Scanner(System.in);
-            Scanner scannerInt = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
+        Scanner scannerInt = new Scanner(System.in);
 
-            try{
-                iloscTestow = scannerInt.nextInt();
-            }catch(InputMismatchException e){
-                System.out.printf("Blad");
-            }
+        try{
+            iloscTestow = scannerInt.nextInt();
+        }catch(InputMismatchException e){
+            System.out.printf("Blad");
+        }
 
+        if (iloscTestow <= 100) {
 
-            if (iloscTestow <= 100) {
-                tab = new String[iloscTestow];
-                for (int i = 0; i < iloscTestow; i++) {
-
-                    try {
-                        pesel = scanner.nextLine();
-                        tab[i] = pesel;
-                    }catch (Exception e){
-                        System.out.printf("Blad");
-                    }
+            tab = new String[iloscTestow];
+            for (int i = 0; i < iloscTestow; i++) {
+                try {
+                    pesel = scanner.nextLine();
+                    tab[i] = pesel;
+                }catch (Exception e){
+                    System.out.printf("Blad");
                 }
             }
 
@@ -56,7 +54,9 @@ public class PESEL {
 
             }
 
-            scanner.close();
-            scannerInt.close();
+        }
+
+        scanner.close();
+        scannerInt.close();
     }
 }
