f18e49ece3e5cf4e6d04d74e71d27dc224f59f39
JWalendziak <92077652+JWalendziak@users.noreply.github.com>
2021-11-04 18:43:13 +0100

    Add files via upload

---
+++ JanWalendziak/1077_BajtlandzkaWojenka.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _1077_BajtlandzkaWojenka
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] tablicaWejciowa = PobranieDanych();
+            List<int> posortowane = SortowanieDanych(tablicaWejciowa);
+            List<int> tablicaPorownawcza = TablicaPorownawcza(posortowane);
+            ObliczanieZwyciestwa(posortowane, tablicaPorownawcza);
+        }
+
+        public static string[] PobranieDanych()
+        {
+            string input = Console.ReadLine();
+            string[] tab = input.Split(" ");
+
+            return tab;
+        }
+
+        public static List<int> SortowanieDanych(string[] tab)
+        {
+            List<int> liczby = new List<int>();
+            for (int i = 0; i < tab.Length; i++)
+            {
+                int liczba = Convert.ToInt32(tab[i]);
+                liczby.Add(liczba);
+            }
+            liczby.Sort();
+
+            return liczby;
+        }
+
+        public static List<int> TablicaPorownawcza(List<int> posortowane)
+        {
+            List<int> drugiZbior = new List<int>();
+            for (int i = 1; i <= 52; i++)
+            {
+                if (!posortowane.Contains(i))
+                {
+                    drugiZbior.Add(i);
+                }
+            }
+            drugiZbior.Sort();
+
+            return drugiZbior;
+        }
+
+        public static void ObliczanieZwyciestwa(List<int> list1, List<int> list2)
+        {
+            int counter = 0;
+
+            for (int i = list1.Count - 1; i > 0; i--)
+            {
+                for (int j = list2.Count - 1; j > 0; j--)
+                {
+                    if (list1[i] > list2[j])
+                    {
+                        counter++;
+                        break;
+                    }
+                }
+            }
+            Console.WriteLine(counter);
+        }
+    }
+}
---
+++ JanWalendziak/703_ProblemCollatza.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace taks_703_ProblemCollatza
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int t = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < t; i++)
+            {
+                int s = int.Parse(Console.ReadLine());
+
+                Console.WriteLine(Wynik(s, 0)); 
+            }
+        }
+
+        private static int Wynik(int s, int n)
+        {
+            int n_wyraz;
+            if (s == 1)
+            {
+                return n;
+            }
+            else
+            {
+                if ( s % 2 == 1)
+                {
+                    return Wynik(3 * s + 1, ++n);
+                }
+                else
+                {
+                     return Wynik(s / 2, ++n);
+                }
+            }
+        }
+    }
+}
