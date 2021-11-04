0f700a1ce8d18e9b9a409a135788b790e230b6b1
Mamin-simpotiaga <oliferuk21@gmail.com>
2021-11-04 13:55:47 +0100

    spoj Oliferuk

---
+++ ArturOliferuk/NWD.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NWD
+{
+    class Program
+    {
+        static int nwd(int a, int b)
+        {
+            if (a == 0)
+            {
+                return b;
+            }
+            else
+            {
+                while (b != 0)
+                {
+                    if (a > b)
+                    {
+                        a -= b;
+                    }
+                    else
+                    {
+                        b -= a;
+                    }
+                }
+
+                return a;
+            }
+        }
+        static void Main(string[] args)
+        {
+            int t = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < t; i++)
+            {
+                var temp = Console.ReadLine().Split().Select(s=>int.Parse(s)).ToList();
+                Console.WriteLine(nwd(temp[0],temp[1]));
+            }
+
+        }
+    }
+}
