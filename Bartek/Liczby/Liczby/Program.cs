05ae7a2f4a3e255f1f1d07e2705e93aec3b74bd0
Bartosz666 <92077699+Bartosz666@users.noreply.github.com>
2021-11-04 16:30:45 +0100

    Add files via upload

---
+++ Bartek/Liczby/Liczby/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Liczby
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int l = int.Parse(Console.ReadLine());
+            string wynik;
+
+            for ( int i = 0; i < l; i++)
+            {
+               wynik = LiczbaPierwsza(int.Parse(Console.ReadLine())) == true ? "Tak" : "Nie";
+                Console.WriteLine(wynik);
+            }
+
+            Console.ReadKey();
+        }
+
+        private static bool LiczbaPierwsza(int c)
+        {
+            if (c == 1)
+            {
+                return false;
+            }
+            for (int i = 2; i < (c/2)+1; i++)
+            {
+                if (c % i == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
