b518e9571a84d318adc6f697d1b27045afee4af2
MateuszGowin <gowmati@gmail.com>
2021-11-04 18:44:00 +0100

    lista+3 zadania

--- MateuszGowin/flamaster.cpp
+++ MateuszGowin/flamaster.cpp
@@ -1,33 +1,56 @@
 #include <iostream>
 #include <string>
 using namespace std;
-int main(){
+int main()
+{
     int c;
     cin>>c;
     if(c<0 || c>50)
         return 0;
+    string tab[c];
     string s;
-    cin>>s;
-    if(s.size()>200)
-        return 0;
-    int wsk=0;
-    int i=0;
-    int licznik=0;
-    while(i<s.size()){
-        wsk=i+2;
-        licznik=2;
-        while(wsk<s.size() && s[i]==s[wsk]){
-            //cout<<"s["<<i<<"]:"<<s[i]<<" s["<<wsk<<"]:"<<s[wsk]<<endl;
-            licznik++;
-            wsk++;
+    for(int i=0;i<c;i++){
+        cin>>s;
+        if(s.size()>200)
+            return 0;
+        else
+        {
+            for(int j=0;j<s.size();j++)
+            {
+                if(s[j]<'A' || s[j]>'Z')
+                    return 0;
+            }
+        }
+        tab[i]=s;
+    }
 
+    for(int z=0; z<c; z++)
+    {
+        int wsk=0;
+        int i=0;
+        int licznik=0;
+        while(i<tab[z].size())
+        {
+            wsk=i+2;
+            licznik=2;
+            while(wsk<tab[z].size() && tab[z][i]==tab[z][wsk])
+            {
+                licznik++;
+                wsk++;
+            }
+            if(licznik>2)
+            {
+                cout<<tab[z][i]<<licznik;
+                i=wsk;
+            }
+            else
+            {
+                cout<<tab[z][i];
+                i++;
+            }
         }
-        if(licznik>2){
-            cout<<s[i]<<licznik<<endl;
-            i=wsk;
-        }else{
-            i++;
-        }
+        if(z+1!=c)
+        cout<<endl;
     }
     return 0;
 }
