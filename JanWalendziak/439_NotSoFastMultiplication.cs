7aa3b2c495e7fd5268a931067d05f700e4614768
JWalendziak <92077652+JWalendziak@users.noreply.github.com>
2021-11-04 18:59:11 +0100

    Add files via upload (#129)

---
+++ JanWalendziak/439_NotSoFastMultiplication.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+namespace _439_NotSoFastMultiplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader sr = new StreamReader("dane.txt");
+
+            int testy = Convert.ToInt32(sr.ReadLine()) + 1;
+            for (int i = 1; i < testy; i++)
+            {
+                string[] line = sr.ReadLine().Split(" ");
+                int liczba1 = Convert.ToInt32(line[0]);
+                int liczba2 = Convert.ToInt32(line[1]);
+                int wynik = liczba1 * liczba2;
+                Console.WriteLine(wynik);
+                using (StreamWriter sw = File.AppendText("output.txt"))
+                {
+                    sw.WriteLine(wynik);
+
+                }
+            }
+        }
+    }
+}
---
+++ JanWalendziak/Sumy.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Sumy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = new List<int>();
+            int t = int.Parse(Console.ReadLine());
+            for (int i = t - 1; i >= 0; i++)
+            {
+                int input = int.Parse(Console.ReadLine());
+                Sumy(numbers, 1, input);
+            }
+        }
+
+        private static void Sumy(List<int> numbers, int l, int s)
+        {
+            if (s == 0)
+            {
+                foreach (var item in numbers)
+                {
+                    Console.WriteLine(item);
+                }
+                Console.WriteLine();
+                return;
+            }
+
+            for (int j = l; s - j >= 0; j++)
+            {
+                numbers.Add(j);
+                Sumy(numbers, j, s - j);
+                numbers.RemoveAt(numbers.Count - 1);
+
+            }
+        }
+    }
+}
