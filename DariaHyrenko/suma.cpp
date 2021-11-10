84c5720b6c4ef9a6cef9669f9a05aa98eedb955b
Daria Hyrenko <lilikon787@gmail.com>
2021-11-10 18:43:49 +0100

    znikli mi < i > dla sumy ;-;

--- DariaHyrenko/suma.cpp
+++ DariaHyrenko/suma.cpp
@@ -1,16 +1,16 @@
-#include iostream
-#include vector
+#include <iostream>
+#include <vector>
 using namespace std;
 
-void suma(vector int tab, int k, int s) {
+void suma(vector <int> tab, int k, int s) {
     if(s == 0) {
-        for(int i = 0; i  tab.size(); i++) {
-            cout  tab[i]   ;
+        for(int i = 0; i < tab.size(); i++) {
+            cout << tab[i];
         }
-        cout<<endl;
+        cout << endl;
         return;
     }
-    for(int i = k; s - i = 0; i++) {
+    for(int i = k; s - i >= 0; i++) {
         tab.push_back(i);
         suma(tab, i, s - i);
         tab.pop_back();
@@ -18,12 +18,12 @@ void suma(vector int tab, int k, int s) {
 }
 
 int main() {
-    vector int tab;
+    vector <int> tab;
     int t, n;
 
-    cin  t;
+    cin >> t;
     while(t--) {
-        cin  n;
+        cin >> n;
         suma(tab, 1, n);
     }
 }
