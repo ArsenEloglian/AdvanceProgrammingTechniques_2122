e805e318a72e1f7073a74756886097fa9801b9de
michple <48492099+michple@users.noreply.github.com>
2021-11-17 16:23:16 +0100

    [Michał Plewa] 10.11.2021 Zadania spoj (#141)

---
+++ Michal_Plewa/cpp/522.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int main() {
+    int n;
+    cin>>n;
+    for(int i=0; i<n; ++i) {
+        int a,b;
+        cin>>a;
+        cin>>b;
+        if(10<=a&&b<=30) {
+            int m=a*b;
+            while (b!=0) {
+                int c = a % b;
+                a = b;
+                b = c;
+            }
+            cout<<m/a;
+        } else {
+            return -1;
+        }
+        cout<<"\n";
+    }
+    return 0;
+}
