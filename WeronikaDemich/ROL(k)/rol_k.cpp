0739b07503c9354bd1af1b073ad827d124e03231
kamgaj <62263284+kamgaj@users.noreply.github.com>
2021-11-18 19:11:36 +0100

    Update rol_k.cpp

--- WeronikaDemich/ROL(k)/rol_k.cpp
+++ WeronikaDemich/ROL(k)/rol_k.cpp
@@ -1,21 +1,15 @@
 //https://pl.spoj.com/problems/PP0602D/
 #include <iostream>
-
+using namespace std;
 int main()
 {
     int counter, shift;
-    std::cin >> counter >> shift;
+    cin >> counter >> shift;
     int tab[counter];
-
     for(int i = 0; i < counter; i++) {
-        std::cin >> tab[i];
-    }
-    for (int i = shift; i < counter; i++)
-    {
-        std::cout << tab[i]<< ' ';
-    }
-    for (int i = 0; i < shift; i++)
-    {
-        std::cout << tab[i] << ' ';
-    }
+        cin >> tab[i]; }
+    for (int i = shift; i < counter; i++) {
+        cout << tab[i]<< ' '; }
+    for (int i = 0; i < shift; i++) {
+        cout << tab[i] << ' '; }
 }
