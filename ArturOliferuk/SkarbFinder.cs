678d999dd1c297e4573f48c92b5bda87c0096fe2
Artur Oliferuk <45690506+Mamin-simpotiaga@users.noreply.github.com>
2021-11-18 20:17:53 +0100

    [Artur Oliferuk] Spoj (#147)
    * lista obecnosci
    
    * spoj Oliferuk
    
    * Lista obecnosci Oliferuk
    
    * Lista obecnosci Oliferuk
    
    * Spoj Oliferuk

---
+++ ArturOliferuk/SkarbFinder.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkarbFinder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int t = int.Parse(Console.ReadLine());
+            int x = 0, y = 0;
+            for (int i = 0; i < t; i++)
+            {
+                int n = int.Parse(Console.ReadLine());
+                for (int j = 0; j < n; j++)
+                {
+                    int[] temp = Console.ReadLine().Split().Select(s => int.Parse(s)).ToArray();
+                    switch (temp[0])
+                    {
+                        case 0:
+                            y += temp[1];
+                            break;
+                        case 1:
+                            y -= temp[1];
+                            break;
+                        case 2:
+                            x += temp[1];
+                            break;
+                        case 3:
+                            x -= temp[1];
+                            break;
+                    }
+                }
+                if (x == 0 && y == 0)
+                {
+                    Console.WriteLine("studnia");
+                }
+                else
+                {
+                    if (y > 0)
+                    {
+                        Console.WriteLine("0 " + y);
+                    }
+                    if (y < 0)
+                    {
+                        Console.WriteLine("1 " + -y);
+                    }
+                    if (x > 0)
+                    {
+                        Console.WriteLine("2 " + x);
+                    }
+                    if (x < 0)
+                    {
+                        Console.WriteLine("3 " + -x);
+                    }
+                }
+                x = 0; y = 0;
+            }
+        }
+    }
+}
