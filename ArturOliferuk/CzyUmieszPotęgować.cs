3de646e15d0bd860efca59b8454034bce26b4be0
kamgaj <62263284+kamgaj@users.noreply.github.com>
2021-11-25 19:06:17 +0100

    Update CzyUmieszPotęgować.cs (#191)

--- ArturOliferuk/CzyUmieszPotęgować.cs
+++ ArturOliferuk/CzyUmieszPotęgować.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,7 +23,6 @@ namespace Potęg
                 }
                 Console.WriteLine(a % 10);
             }
-            
             Console.Read();
         }
     }
