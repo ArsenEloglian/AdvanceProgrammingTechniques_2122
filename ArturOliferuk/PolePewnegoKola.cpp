24862b2fef32248624f836e739842b2cdee85d92
kamgaj <62263284+kamgaj@users.noreply.github.com>
2021-11-25 19:16:33 +0100

    Update PolePewnegoKola.cpp (#220)
    Co-authored-by: Artur Oliferuk <45690506+Mamin-simpotiaga@users.noreply.github.com>

--- ArturOliferuk/PolePewnegoKola.cpp
+++ ArturOliferuk/PolePewnegoKola.cpp
@@ -1,17 +1,15 @@
 #include <iostream>
 #include <ctime>
- 
 using namespace std;
  
 int main(){
     int q, i;
-cin>>q;
-for (int j=0; j<q; ++j)
-{
-    cin>>i;
-    if (i==1) {cout<<"NIE"<<endl;}
-    if (i==2) {cout<<"TAK"<<endl;}
-
+    cin>>q;
+    for (int j=0; j<q; ++j)
+    {
+        cin>>i;
+        if (i==1) {cout<<"NIE"<<endl;}
+        if (i==2) {cout<<"TAK"<<endl;}
         int l;
         for(l=2; l<i; ++l)
         {
@@ -25,5 +23,6 @@ for (int j=0; j<q; ++j)
                 cout<<"TAK"<<endl;
             }
         }
+    }
 }
 }
