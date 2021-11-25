cfc28c9debaceaab75e53fe6c21c27c1d156b8e6
Artur Oliferuk <45690506+Mamin-simpotiaga@users.noreply.github.com>
2021-11-25 16:59:25 +0100

    [Artur Oliferuk] Spoj 5  (#176)
    * lista obecnosci
    
    * spoj Oliferuk
    
    * Lista obecnosci Oliferuk
    
    * Lista obecnosci Oliferuk
    
    * Spoj Oliferuk
    
    * [Oliferuk] spoj

---
+++ ArturOliferuk/ROL.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ROL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int t = int.Parse(Console.ReadLine());
+            for (int i = 0; i < t; i++)
+            {
+                var input = Console.ReadLine().Split().Select(s => int.Parse(s)).ToArray();
+                int[] result = new int[input[0]];
+                result[input[0] - 1] = input[1];
+                for (int j = 2; j <= input[0]; j++)
+                {
+                    result[j - 2] = input[j];
+                }
+                for (int j = 0; j < result.Length; j++)
+                {
+                    Console.Write(result[j]);
+                    if (j < result.Length - 1)
+                        Console.Write(" ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
