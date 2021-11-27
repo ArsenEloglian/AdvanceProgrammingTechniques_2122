34b2ce06adca8ab6fd58306fc10f0c9351cbe07c
pawelk98 <48221507+pawelk98@users.noreply.github.com>
2021-11-27 10:22:54 +0100

    [Pawel kumorowski] poprawa czytelności kodu (#240)
    * [Paweł Kumorowski] poprawa czytelności kodu
    
    * [Paweł Kumorowski] poprawa czytelności kodu
    
    * [Paweł Kumorowski] poprawa czytelności kodu; zmiana pętli while na for
    
    * [Paweł Kumorowski] poprawa czytelności kodu; poprawienie pętli for
    
    * [Paweł Kumorowski] poprawa czytelności kodu

--- Kamil_Olejarz/proste_dodawanie.c
+++ Kamil_Olejarz/proste_dodawanie.c
@@ -1,39 +1,21 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-int main(){
-	
-	int i; 
-	int j; 
-	int liczba; 
-	int suma; 
-	int liczbaTestow; 
-	int n; 	
-
+int main() {
+	int i, j, n, liczba, suma, liczbaTestow;
 
 	scanf("%d", &liczbaTestow);
-	
-	for(i=0; i<liczbaTestow;i++){
-		
-		
-		n = 0; 
-		liczba = 0;
-		suma  = 0; 
-		
-		
+
+	for (i = 0; i < liczbaTestow; i++) {
+		n = liczba = suma = 0;
+
 		scanf("%d", &n);
-		for(j=0; j<n;j++){
-			
+		
+		for (j = 0; j < n; j++) {
 			scanf("%d", &liczba);
-			
-			suma = suma + liczba; 				
+			suma = suma + liczba;
 		}
-		
-		printf("%d\n", suma); 
-		
-		
-		
+
+		printf("%d\n", suma);
 	}
-	
-	return 0; 	
 }
