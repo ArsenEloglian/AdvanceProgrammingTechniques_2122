206ec16c520351a5c2bcc8acbd685263f4f85916
JWalendziak <92077652+JWalendziak@users.noreply.github.com>
2021-12-09 18:19:47 +0100

    Add files via upload

---
+++ JanWalendziak/499_CzyUmieszPotegowac.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace CzyUmieszPotegowac_499
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfOperations = Convert.ToInt32(Console.ReadLine());
+
+            if (numberOfOperations >= 1 && numberOfOperations <= 10)
+            {
+                for (int i = 0; i < numberOfOperations; i++)
+                {
+                    string input = Console.ReadLine();
+                    string[] separatedInput = input.Split(" ");
+                    int @base = Convert.ToInt32(separatedInput[0]);
+                    @base = @base % 10;
+                    int exponent = Convert.ToInt32(separatedInput[1]);
+
+                    if (@base <= 1000000000 && @base >= 1 && exponent <= 1000000000 && exponent >= 1)
+                    {
+                        if (exponent % 4 == 0)
+                        {
+                            exponent = 4;
+                        }
+                        else
+                        {
+                            exponent = exponent % 4;
+                        }
+                        int result = @base;
+                        for (int j = 1; j < exponent; j++)
+                        {
+                            result *= @base;
+                        }
+                        Console.WriteLine(result % 10);
+                    }
+                }
+
+            }
+
+        }
+
+
+    }
+}
---
+++ JanWalendziak/506_Flamaster.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Flamaster_506
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfOperations = Convert.ToInt32(Console.ReadLine());
+
+            if (numberOfOperations >= 1 && numberOfOperations <= 50)
+            {
+                for (int i = 0; i < numberOfOperations; i++)
+                {
+                    IDictionary<char, int> numChar = new Dictionary<char, int>();
+                    string input = Console.ReadLine();
+
+                    foreach (char c in input)
+                    {
+                        if (numChar.ContainsKey(c))
+                        {
+                            int counter = numChar[c];
+                            numChar[c] = ++counter;
+                        }
+                        else
+                        {
+                            numChar.Add(c, 1);
+                        }
+                    }
+
+                    string output = "";
+                    foreach (KeyValuePair<char, int> kvp in numChar)
+                    {
+                        if (kvp.Value == 1)
+                        {
+                            output += $"{kvp.Key}";
+                        }
+                        else if (kvp.Value == 2)
+                        {
+                            output += $"{kvp.Key}{kvp.Key}";
+                        }
+                        else
+                        {
+                            output += $"{kvp.Key}{kvp.Value}";
+                        }
+                    }
+
+                    Console.WriteLine(output);
+                }
+            }
+        }
+    }
+}
