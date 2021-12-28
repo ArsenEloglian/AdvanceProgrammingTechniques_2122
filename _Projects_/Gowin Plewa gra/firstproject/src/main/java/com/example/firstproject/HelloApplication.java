a6762e17ad4b939f872572e07813262bb52a5897
michple <michaple1997@gmail.com>
2021-12-28 22:21:55 +0100

    Dodanie logiki do gry

--- _Projects_/Gowin Plewa gra/firstproject/src/main/java/com/example/firstproject/HelloApplication.java
+++ _Projects_/Gowin Plewa gra/firstproject/src/main/java/com/example/firstproject/HelloApplication.java
@@ -20,8 +20,86 @@ public class HelloApplication extends Application {
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
-        
+        Plansza arr = new Plansza(10);
+        //gra
+        // player1
+        while (true) {
+            System.out.println("Gracz 1");
+            if (!playerMove(arr, arr.player1)) {
+                System.out.println("Gracz 2 wygrał");
+                break;
+            }
+            System.out.println("Gracz 2");
+            if (!playerMove(arr, arr.player2)) {
+                System.out.println("Gracz 1 wygrał");
+                break;
+            }
+        }
 
     }
+
+    public boolean playerMove(Plansza arr, Player player) {
+        printArray(arr.arr);
+        if (!arr.isPlayerHaveMove(player)) {
+            return false;
+        }
+        System.out.print("KRÓLOWA: ");
+        Point quennPoint;
+        do {
+            quennPoint = podaj_wspolrzedne();
+        } while (!arr.checkIsPointIsQueen(quennPoint, player) && !arr.ifQueenHaveMove(quennPoint, player));
+        System.out.print("RUCH KRÓLOWEJ: ");
+        Point quennMovePoint;
+        do {
+            quennMovePoint = podaj_wspolrzedne();
+        } while (!arr.ifMoveIsPosible(quennMovePoint, quennPoint, player));
+        arr.movequeen(quennPoint, quennMovePoint, player);
+        printArray(arr.arr);
+        System.out.print("BLOK: ");
+        Point blockPoint;
+        do {
+            blockPoint = podaj_wspolrzedne();
+        } while (!arr.ifBlockIsPosible(blockPoint, quennMovePoint, player));
+        arr.setPoint(blockPoint, Global.block);
+        printArray(arr.arr);
+        return true;
+
+    }
+
+
+    private Point podaj_wspolrzedne() {
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            System.out.print("Podaj wspolrzedne");
+            String s = sc.next();
+            if (s.length() == 2) {
+                if (s.charAt(0) > 64 && s.charAt(0) < 75 && s.charAt(1) > 47 && s.charAt(1) < 57) {
+                    int i = s.charAt(0) - 65;
+                    int j = s.charAt(1) - 48;
+                    return new Point(j, i);
+                }
+            }
+        }
+    }
+
+    private void printArray(String[][] arr) {
+        System.out.print("  ");
+        for (int i = 0; i < 10; i++) {
+            int charcode = i + 65;
+            System.out.print((char) charcode + " ");
+        }
+        System.out.println();
+        for (int i = 0; i < 10; i++) {
+            System.out.print(i + " ");
+            for (int j = 0; j < 10; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+
 }
-   
