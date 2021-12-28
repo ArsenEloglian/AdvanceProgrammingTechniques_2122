d04729a00b356d2418d7484e99bcd3e5c9eaa7e0
michple <michaple1997@gmail.com>
2021-12-28 22:21:55 +0100

    Dodanie logiki do gry

---
+++ _Projects_/Gowin Plewa gra/firstproject/src/main/java/com/example/firstproject/Global.java
@@ -0,0 +1,10 @@
+package com.example.firstproject;
+
+
+
+public class Global {
+
+    public final static String queen1 = "q";
+    public final static String queen2 = "Q";
+    public final static String block = "B";
+}
