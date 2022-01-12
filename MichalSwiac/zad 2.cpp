37b2bd0dcae17a7227d64f409ef2e76a6ac83bba
hanzo912 <dominiklesniewski@tlen.pl>
2022-01-12 21:46:27 +0100

    Revert "Merge branch 'ArsenEloglian:main' into work"
    This reverts commit a46c2dc3cf49935ab6306f611f730fabefe60bec, reversing
    changes made to ea7c3911654631bf2cf00ace262dc9c2588de2b4.

--- MichalSwiac/zad 2.cpp
+++ MichalSwiac/zad 2.cpp
@@ -2,20 +2,19 @@
 
 //PRIME_T - Liczby Pierwsze
 
-using namespace std;
 
+using namespace std;
 int ileLiczb;
 int podanaLiczba;
 bool liczbaPierwsza;
-
 int main(){
 
     cin>> ileLiczb;
     int liczbyDoSprawdzenia[ileLiczb];
 
-    for(int i=0;i<ileLiczb; i++)
+    for(int i=0;i<ileLiczb; i++){
         cin>> liczbyDoSprawdzenia[i];
-    
+    }
     for(int i=0;i<ileLiczb; i++){
         podanaLiczba=liczbyDoSprawdzenia[i];
         liczbaPierwsza = true;
@@ -25,15 +24,20 @@ int main(){
             liczbaPierwsza = false;
         }
 
-        for(int dzielnik=2 ; dzielnik <= podanaLiczba/2; dzielnik++)
+        for(int dzielnik=2 ; dzielnik <= podanaLiczba/2; dzielnik++){
             if(podanaLiczba%dzielnik==0){
                 cout<< "NIE" <<endl;
                 liczbaPierwsza = false;
                 break;
             }
-        
-        if (liczbaPierwsza==true)
+        }
+
+        if (liczbaPierwsza==true){
             cout<< "TAK" <<endl;
+        }
+
     }
+
+
 }
 
